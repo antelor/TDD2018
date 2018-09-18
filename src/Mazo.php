6d3d03bbb022170227231391eb52f09451d9f3b9
antelor <42157966+antelor@users.noreply.github.com>
2018-09-18 17:03:01 -0300

    Codigo de mezclar

--- src/Mazo.php
+++ src/Mazo.php
@@ -3,14 +3,23 @@
 namespace TDD;
 
 class Mazo {
+	protected $cartas;
+
+	public function __construct($array){
+		$this->cartas = $array;	
+	}
+
   public function mezclar() {
+		shuffle($this->cartas);
     return TRUE;
   }
 
 	public function primero() {
+		return $this->cartas[0];
   }
 
 	public function ultimo() {
+		return array_reverse($this->cartas)[0];
   }
 
 
