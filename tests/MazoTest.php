a6b424492fe47f987331fb4df034e6db77a351a5
Ramiro Antelo <ramiro.antelo4@gmail.com>
2018-09-22 12:41:40 -0300

    cambios al test de cortar

--- tests/MazoTest.php
+++ tests/MazoTest.php
@@ -25,20 +25,10 @@ class MazoTest extends TestCase {
 
     }
 
-    public function testCortarPar() {
-	$mazo = new Mazo([0,1,2,4]);
-	$mazoViejo = $mazo->getCartas();
-	
-	$this->assertTrue($mazo->cortar());
-	$this->assertEquals($mazo->getCartas(), [2,4,0,1]);
+    public function testCortar() {
+        $mazo = new Mazo([0,1,2,4]);
+        $mazoViejo = $mazo->getCartas();
+        
+        $this->assertTrue($mazo->cortar());
     }
-    
-    public function testCortarImpar() {
-	$mazo = new Mazo([0,1,2,4,5]);
-	$mazoViejo = $mazo->getCartas();
-	
-	$this->assertTrue($mazo->cortar());
-	$this->assertEquals($mazo->getCartas(), [4,5,0,1,2]);
-    }
-
 }
