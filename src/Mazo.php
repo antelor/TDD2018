16b6973f392150374a4b1a70a2076475730805b0
Ramiro Antelo <ramiro.antelo4@gmail.com>
2018-09-22 13:23:23 -0300

    codigo cortar

--- src/Mazo.php
+++ src/Mazo.php
@@ -25,4 +25,16 @@ class Mazo {
 	public function hayCartas() {
 		return ( $this->cantCartas() > 0 );
 	}
+
+	public function cortar(){
+		if( $this->hayCartas() ){
+			$mazo = $this->getCartas();
+			$randint = rand(1, $this->cantCartas-1 );
+
+			$submazo1 = array_slice($mazo, 0, $randint-1);
+			$submazo2 = array_slice($mazo, $randint);
+
+			$this->cartas = $submazo1 + $submazo2;
+		}
+	}
 }
