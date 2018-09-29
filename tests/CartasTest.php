7402cb1cee5440cee739e121ab5041f090802aa5
antelor <ramiro.antelo4@gmail.com>
2018-09-29 20:05:52 -0300

    el test de crear fue reducido en algunas lineas

--- tests/CartasTest.php
+++ tests/CartasTest.php
@@ -10,23 +10,9 @@ class CartasTest extends TestCase {
         //Cartas espanolas
         $cartaE = new CartaSpanish(12, 'basto');
         $this->assertTrue(isset($cartaE));
-        
-        try {
-            $cartaE2 = new CartaSpanish(13, 'asdg');
-        }
-        catch(Exception $e) {
-            $this->assertTrue( isset($e) );
-        }
 
         //Cartas poker
         $cartaP = new CartaSpanish(5, 'treboles');
         $this->assertTrue(isset($cartaP));
-
-        try {
-            $cartaP2 = new CartaSpanish(13, 'asdg');
-        }
-        catch(Exception $e2) {
-            $this->assertTrue( isset($e2) );
-        }
     }
 }
