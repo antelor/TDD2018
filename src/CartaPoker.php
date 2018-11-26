fa84be8e8fad431d8cac0e8e0be3e7f4824f5fe5
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2018-11-26 01:29:38 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/CartaPoker.php
+++ src/CartaPoker.php
@@ -3,30 +3,30 @@
 namespace TDD;
 use Exception;
 
-class CartaPoker implements CartaInterface{
+class CartaPoker implements CartaInterface {
     protected $numero;
     protected $palo;
     
-    public function __construct($numero, $palo){
+    public function __construct($numero, $palo) {
         $this->numerosPermitidos = ['A', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
         $this->palosPermitidos = ['corazones', 'diamantes', 'treboles', 'picas'];
         
-        if( in_array($numero, $this->numerosPermitidos) && in_array($palo, $this->palosPermitidos) ){
+        if (in_array($numero, $this->numerosPermitidos) && in_array($palo, $this->palosPermitidos)) {
             $this->numero = $numero;
             $this->palo = $palo;
         }
-        else{
+        else {
             throw new Exception("Parametros de constructor no validos para el tipo de carta");
         }
 
         return TRUE;
     }
     
-    public function getNumero(){
+    public function getNumero() {
         return $this->numero;
     }
 
-    public function getPalo(){
+    public function getPalo() {
         return $this->palo;
     }
 }
--- src/CartaSpanish.php
+++ src/CartaSpanish.php
@@ -3,31 +3,31 @@
 namespace TDD;
 use Exception;
 
-class CartaSpanish implements CartaInterface{
+class CartaSpanish implements CartaInterface {
     protected $numero;
     protected $palo;
     
-    public function __construct($numero, $palo){
+    public function __construct($numero, $palo) {
         $this->numerosPermitidos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
         $this->palosPermitidos = ['espadas', 'copas', 'oro', 'basto'];
         
         
-        if( in_array($numero, $this->numerosPermitidos) && in_array($palo, $this->palosPermitidos) ){
+        if (in_array($numero, $this->numerosPermitidos) && in_array($palo, $this->palosPermitidos)) {
             $this->numero = $numero;
             $this->palo = $palo;
         }
-        else{
+        else {
             throw new Exception("Parametros de constructor no validos para el tipo de carta");
         }
         
         return TRUE;
     }
     
-    public function getNumero(){
+    public function getNumero() {
         return $this->numero;
     }
 
-    public function getPalo(){
+    public function getPalo() {
         return $this->palo;
     }
 }
